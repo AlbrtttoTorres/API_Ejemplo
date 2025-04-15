aa0d9f17e0d3ce93d8fbc3fee0895dd099723001
Javier Vidal Molina <javidalin@gmail.com>
2025-04-15 11:40:30 +0200

    .

---
+++ Kikes/Kikes/API.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+@app.route("/")
+def index():
+    return jsonify({
+        "mensaje": "API para predecir average_rating de libros.",
+        "uso": {
+            "GET /predict?data=ratings_count,text_reviews_count,num_pages": "Devuelve una predicción",
+            "POST /predict_json": {
+                "formato": "{'data': [ratings_count, text_reviews_count, num_pages]}"
+            }
+        }
+    })
+
+@app.route("/predict")
+def predict():
+    data_str = request.args.get("data")
+    if not data_str:
+        return jsonify({"error": "Proporciona datos separados por comas"}), 400
+    try:
+        values = np.array([float(x) for x in data_str.split(",")]).reshape(1, -1)
+        prediction = model.predict(values)
+        return jsonify({"prediction": prediction.tolist()[0]})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/predict_json", methods=["POST"])
+def predict_json():
+    try:
+        input_data = request.get_json()
+        values = np.array(input_data["data"]).reshape(1, -1)
+        prediction = model.predict(values)
+        return jsonify({"prediction": prediction.tolist()[0]})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Kikes/Kikes/model.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import pickle
+
+df = pd.read_csv("books.csv", on_bad_lines="skip")
+
+features = ["ratings_count", "text_reviews_count","  num_pages"]
+df = df.dropna(subset=features + ["average_rating"])
+
+
+X = df[features]
+y = df["average_rating"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
+
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
