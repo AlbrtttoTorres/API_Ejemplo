1af3c89427b5d10cec0057632be34859aed2abcb
Javier Vidal Molina <javidalin@gmail.com>
2025-04-16 11:38:47 +0200

    .

--- API.py
+++ API.py
@@ -45,5 +45,5 @@ def predict_json():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
